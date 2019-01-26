b154b40d17d4bb80273b5e0d111eea62a3ba21cd
任鸣 <renming@lianzai.com>
2019-01-26 17:09:03 +0800

    验证码错误处理，读取文件

--- blog.py
+++ blog.py
@@ -52,82 +52,114 @@ def getSentMessageStatus(phone):
         if '手机号不在线或手机号已释放，可尝试检查发码是否成功。' in response:
             return flag
 
+def sendmessage(user, password, name):
+        url = 'https://login.sina.com.cn/signup/signin.php'
 
-url = 'https://login.sina.com.cn/signup/signin.php'
+        chrome_options = Options()
+        # 在Linux需要禁用sandbox
+        chrome_options.add_argument('--no-sandbox')
+        # 谷歌文档提到需要加上这个属性来规避bug
+        # chrome_options.add_argument('--disable-gpu')
+        # 隐藏滚动条, 应对一些特殊页面
+        # chrome_options.add_argument('--hide-scrollbars')
+        # 不加载图片, 提升速度
+        # chrome_options.add_argument('blink-settings=imagesEnabled=false')
+        # https安全问题
+        chrome_options.add_argument('--ignore-certificate-errors')
+        # chrome_options.add_argument('--headless')
+        # 添加代理
+        # chrome_options.add_argument("--proxy-server=socks5://" + PROXY)
+        # 随机user_agent
+        user_agent = random.choice(constants.USER_AGENTS)
+        chrome_options.add_argument('user-agent=%s' % user_agent)
+        # logging.info("opening chrome, catalog_url:%s", catalog_url)
 
-chrome_options = Options()
-# 在Linux需要禁用sandbox
-chrome_options.add_argument('--no-sandbox')
-# 谷歌文档提到需要加上这个属性来规避bug
-# chrome_options.add_argument('--disable-gpu')
-# 隐藏滚动条, 应对一些特殊页面
-# chrome_options.add_argument('--hide-scrollbars')
-# 不加载图片, 提升速度
-# chrome_options.add_argument('blink-settings=imagesEnabled=false')
-# https安全问题
-chrome_options.add_argument('--ignore-certificate-errors')
-# chrome_options.add_argument('--headless')
-# 添加代理
-# chrome_options.add_argument("--proxy-server=socks5://" + PROXY)
-# 随机user_agent
-user_agent = random.choice(constants.USER_AGENTS)
-chrome_options.add_argument('user-agent=%s' % user_agent)
-# logging.info("opening chrome, catalog_url:%s", catalog_url)
+        browser = webdriver.Chrome(chrome_options=chrome_options)
+        browser.get(url)
+        browser.implicitly_wait(3)
+        browser.set_page_load_timeout(10)  # 10秒
+        # wait = WebDriverWait(browser, 30)
 
-browser = webdriver.Chrome(chrome_options=chrome_options)
-browser.get(url)
-browser.implicitly_wait(10)
-wait = WebDriverWait(browser, 30)
+        # name = input('请输入账号：')
+        # # password = input('请输入密码：')
 
-# name = input('请输入账号：')
-# # password = input('请输入密码：')
+        # user = '00639382315692'
+        # password = 'uif698539K'
+        # name = '6934697764'
 
-user = '00639382315692'
-password = 'uif698539K'
-name = '6934697764'
+        browser.find_element("name", "username").send_keys(user)
+        browser.find_element("name", "password").send_keys(password)
+        time.sleep(3)
+        browser.find_elements_by_xpath("//input[@class='W_btn_a btn_34px']")[0].click()
 
-browser.find_element("name", "username").send_keys(user)
-browser.find_element("name", "password").send_keys(password)
-time.sleep(3)
-browser.find_elements_by_xpath("//input[@class='W_btn_a btn_34px']")[0].click()
+        # html = browser.page_source
+        # sel = etree.HTML(html)
+        # captcha_url = sel.xpath("//img[@id='check_img']/@src")
+        while True:
+            check = input('请输入验证码：')
+            browser.find_elements_by_xpath("//input[@id='door']")[0].clear()
+            browser.find_element("name", "door").send_keys(check)
+            browser.find_elements_by_xpath("//input[@class='W_btn_a btn_34px']")[0].click()
+            time.sleep(2)
+            print(browser.current_url)
+            html = browser.page_source
+            sel = etree.HTML(html)
+            check_result = sel.xpath("//span[@class='form_prompt']/i/text()")[0]
+            if check_result != '输入的验证码不正确':
+                break
 
-html = browser.page_source
-sel = etree.HTML(html)
-# captcha_url = sel.xpath("//img[@id='check_img']/@src")
 
-check = input('请输入验证码：')
-browser.find_element("name", "door").send_keys(check)
-browser.find_elements_by_xpath("//input[@class='W_btn_a btn_34px']")[0].click()
 
-url = 'http://control.blog.sina.com.cn/myblog/htmlsource/blog_notopen.php?uid=' + name + '&version=7'
-browser.get(url)
-browser.refresh()
-time.sleep(3)
 
-while True:
-    phone = getphone()
-    browser.find_elements_by_xpath("//input[@id='blogPhoneNum']")[0].clear()
-    browser.find_elements_by_xpath("//input[@id='blogPhoneNum']")[0].send_keys(phone)
-    time.sleep(3)
-    html = browser.page_source
-    sel = etree.HTML(html)
-    try:
-        error = sel.xpath("//p[@id='blogPhoneNumError']/text()")[0]
-        if len(error) > 1:
-            print('号码有错误')
-            phone = getphone()
-            browser.find_elements_by_xpath("//input[@id='blogPhoneNum']")[0].clear()
-            browser.find_elements_by_xpath("//input[@id='blogPhoneNum']")[0].send_keys(phone)
-    except:
-        messaging = sel.xpath("//div[@class='send-msg-tip']/p/text()")[0]
-        message_content = messaging.split('送')[1].split('到')[0]
-        addressee = messaging.split('到')[1].split('进')[0]
+        url = 'http://control.blog.sina.com.cn/myblog/htmlsource/blog_notopen.php?uid=' + name + '&version=7'
+        browser.get(url)
+        browser.refresh()
+        time.sleep(3)
 
-        print(message_content)
-        print(addressee)
+        phone = getphone()
+        browser.find_elements_by_xpath("//input[@id='blogPhoneNum']")[0].clear()
+        browser.find_elements_by_xpath("//input[@id='blogPhoneNum']")[0].send_keys(phone)
+        time.sleep(3)
+        html = browser.page_source
+        sel = etree.HTML(html)
+        while True:
+            try:
+                error = sel.xpath("//p[@id='blogPhoneNumError']/text()")[0]
+                if len(error) > 1:
+                    print('号码有错误')
+                    phone = getphone()
+                    browser.find_elements_by_xpath("//input[@id='blogPhoneNum']")[0].clear()
+                    browser.find_elements_by_xpath("//input[@id='blogPhoneNum']")[0].send_keys(phone)
+            except:
+                messaging = sel.xpath("//div[@class='send-msg-tip']/p/text()")[0]
+                message_content = messaging.split('送')[1].split('到')[0]
+                addressee = messaging.split('到')[1].split('进')[0]
 
-        phone = putSentMessage(phone, message_content, addressee)
-        flag = getSentMessageStatus(phone)
-        # if flag:
-        #     browser.find_elements_by_xpath("//input[@id='blogPhoneNum']")[0].clear()
-        #     browser.find_elements_by_xpath("//input[@id='blogPhoneNum']")[0].send_keys(phone)
+                print(message_content)
+                print(addressee)
+
+                phone = putSentMessage(phone, message_content, addressee)
+                flag = getSentMessageStatus(phone)
+                if flag:
+                    browser.find_elements_by_xpath("//input[@id='blogPhoneNum']")[0].clear()
+                    browser.find_elements_by_xpath("//input[@id='blogPhoneNum']")[0].send_keys(phone)
+                else:
+                    break
+        browser.find_elements_by_xpath("//a[@class='btn']")[0].click()
+
+
+
+
+if __name__ == '__main__':
+    card_number = []
+    with open('blog.txt', 'r') as f:
+        for i in f.readlines():
+            card_number.append(i.replace('卡号:', '').strip())
+
+        for card in card_number:
+            result = card.split('----')
+            user = result[0]
+            password = result[1]
+            name = result[2]
+            sendmessage(user, password, name)
+
